76328a4efac9531492e4a4b226e518653680d503
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-07 23:18:22 +0000

    task 0

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Task 0"""
+
+
+def canUnlockAll(boxes):
+    """method that determines if all the boxes can be opened"""
+    keys = [*range(len(boxes))]
+    unlock_boxes = [0]
+    for key, box in zip(keys, boxes):
+        for k in box:
+            if k < len(boxes) and k not in unlock_boxes and k != key:
+                unlock_boxes.append(k)
+    if len(unlock_boxes) == len(boxes):
+        return True
+    return False
