70b098e25fd5cb6ca684265e306b1dfbfd49e0b0
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-07 23:30:02 +0000

    a

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,10 +7,8 @@ def canUnlockAll(boxes):
     keys = [*range(len(boxes))]
     unlock_boxes = [0]
     for key, box in zip(keys, boxes):
-        if not box:
-            continue
         for k in box:
-            if k < len(boxes) and k not in unlock_boxes and k != key:
+            if (k < len(boxes)) and (k not in unlock_boxes) and (k != key):
                 unlock_boxes.append(k)
     if len(unlock_boxes) == len(boxes):
         return True
