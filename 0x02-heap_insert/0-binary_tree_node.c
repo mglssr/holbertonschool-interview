7b11a2cdd7f4e4dfa4a7aa0fc93d6aef90f03115
Maia Iglesias Rojas <97979014+mglssr@users.noreply.github.com>
2022-12-21 23:27:37 +0000

    task 0

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -14,11 +14,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	node = malloc(sizeof(binary_tree_t));
     if (!node)
-		return (0);
+		return (NULL);
 	(*node).parent = parent;
 	(*node).n = value;
 	(*node).left = NULL;
 	(*node).right = NULL;
-
 	return (node);
 }
